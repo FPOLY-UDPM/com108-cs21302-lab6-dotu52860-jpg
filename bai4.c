68382944762d1ec024792d3a4c3a5208f97ebc86
dotu52860-jpg <dotu52860@gmail.com>
2026-02-25 08:55:02 +0700

    nộp lab

--- bai4.c
+++ bai4.c
@@ -9,4 +9,32 @@
 //  Output: Xuất ra màn hình ma trận bình phương 
 
 // VIẾT CODE Ở ĐÂY
-
+#include <stdio.h>
+int main() {
+    int n, m;
+    
+    // Nhập số hàng và số cột của ma trận
+    printf("Nhap so hang va so cot cua ma tran: ");
+    scanf("%d %d", &n, &m);
+    
+    int matrix[n][m];
+    
+    // Nhập các phần tử của ma trận
+    printf("Nhap cac phan tu cua ma tran:\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+    
+    // Tính bình phương các phần tử trong ma trận
+    printf("Ma tran binh phuong:\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            printf("%d ", matrix[i][j] * matrix[i][j]);
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
